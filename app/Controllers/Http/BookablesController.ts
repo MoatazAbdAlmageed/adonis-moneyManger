0dbbb978f237b2ecca363cca413204340063bb7a
Moataz Mohammady <moataz@wuilt.com>
2021-06-19 23:48:17 +0200

    Bookable CRUD

--- app/Controllers/Http/BookablesController.ts
+++ app/Controllers/Http/BookablesController.ts
@@ -1,7 +1,100 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+import User from "App/Models/User";
+import Customer from "App/Models/Customer";
+import Bookable from "App/Models/Bookable";
 
 export default class BookablesController {
   public async index({ view, request }: HttpContextContract) {
-    return "TODO";
+    const limit = 5;
+    const page = request.input("page", 1);
+    const bookables = await Bookable.query().paginate(page, limit);
+    return view.render("bookables.index", { bookables });
+  }
+  public async show(httpContextContract: HttpContextContract) {
+    const { params, view } = httpContextContract;
+    const users = await User.all();
+    const customers = await Customer.all();
+    const bookable = await Bookable.find(params.id);
+    return view.render("bookables.edit", { bookable, users, customers });
+  }
+  public async add(httpContextContract: HttpContextContract) {
+    const users = await User.all();
+    const customers = await Customer.all();
+    const { view } = httpContextContract;
+    return view.render("bookables.add", { users, customers });
+  }
+
+  public async store({ request, response, session }: HttpContextContract) {
+    /**
+     *  TODO:DRY same code used in update!
+     * try to refactor this !
+     */
+
+    const validationSchema = schema.create({
+      title: schema.string(),
+      description: schema.string(),
+      quantity: schema.number(),
+      price: schema.number(),
+    });
+
+    try {
+      const validationData = await request.validate({
+        schema: validationSchema,
+        messages: {
+          "title.required": "title Is required",
+          "description.required": "description Is required",
+          "quantity.required": "quantity Is required",
+          "price.required": "price Is required",
+        },
+      });
+      Bookable.create(validationData);
+      session.flash("notification", "Bookable Created!");
+      response.redirect("/bookables");
+    } catch (error) {
+      return error;
+      session.flash("error", error.messages);
+      response.redirect("back");
+    }
+  }
+
+  public async update({
+    request,
+    response,
+    session,
+    params,
+  }: HttpContextContract) {
+    const validationSchema = schema.create({
+      title: schema.string(),
+      description: schema.string(),
+      quantity: schema.number(),
+      price: schema.number(),
+    });
+    const validationData = await request.validate({
+      schema: validationSchema,
+      messages: {
+        "title.required": "title Is required",
+        "description.required": "description Is required",
+        "quantity.required": "quantity Is required",
+        "price.required": "price Is required",
+        "details.required": "details Is required",
+      },
+    });
+    const { title, description, quantity, price, details } = validationData;
+    const bookable = await Bookable.findOrFail(params.id);
+    bookable.title = title;
+    bookable.description = description;
+    bookable.quantity = quantity;
+    bookable.price = price;
+    // TODO:save dateOfBirth
+    bookable.save();
+    session.flash("notification", "Bookable Updated!");
+    response.redirect("/bookables");
+  }
+  public async destroy({ response, session, params }: HttpContextContract) {
+    const bookable = await Bookable.findOrFail(params.id);
+    bookable.delete();
+    session.flash("notification", "Bookable Deleted!");
+    response.redirect("/bookables");
   }
 }
--- app/Models/Bookable.ts
+++ app/Models/Bookable.ts
@@ -1,13 +1,28 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class Bookable extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
+
+  @column()
+  public title: string;
+
+  @column()
+  public description: string;
+
+  @column()
+  public quantity: number;
+
+  @column()
+  public price: number;
+
+  @column()
+  public is_deleted: boolean;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
